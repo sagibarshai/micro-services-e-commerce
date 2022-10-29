31eceeed460f90bd4f192897a44f4d594e46a836
Sagi bar-shai <sagibarshai1@gmail.com>
2022-10-29 22:56:01 +0300

    small change

--- auth/src/errors/database-error.ts
+++ auth/src/errors/database-error.ts
@@ -4,6 +4,7 @@ export class DatabaseError extends CustomError {
      statusCode = 500;
      constructor(public message: string) {
           super(message);
+          Object.setPrototypeOf(this, DatabaseError.prototype);
      }
      serializeErrors() {
           return [{ message: this.message }];
